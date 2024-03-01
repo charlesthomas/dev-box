1c895151bdd9db8cbe10beddbe63dd424a8a7059
Charles Thomas <ch@rlesthom.as>
2024-02-29 23:20:54 -0500

    fix: mount $HOME, run as $USER
    just mount the entire home dir, and run as myself to simplify

--- run.sh
+++ run.sh
@@ -13,17 +13,16 @@ PASSWORD=$UUID \
 docker run \
 --rm \
 -p 127.0.0.1:8080:8080 \
--w /home/coder \
--v ${HOME}/code:/home/coder/code:Z \
--v ${HOME}/.bw:/home/coder/.bw:Z \
--v ${HOME}/.kube/config.d:/home/coder/.kube/config.d:Z \
--v ${HOME}/.local/share/code-server:/home/coder/.local/share/code-server:Z \
--v ${HOME}/Library/Application\ Support/Code/User/keybindings.json:/home/coder/.local/share/code-server/User/keybindings.json:Z \
--v ${HOME}/Library/Application\ Support/Code/User/settings.json:/home/coder/.local/share/code-server/User/settings.json:Z \
+-w /home/${USER} \
+-v ${HOME}:/home/${USER}:Z \
+-v ${HOME}/Library/Application\ Support/Code/User/keybindings.json:/home/${USER}/.local/share/code-server/User/keybindings.json:Z \
+-v ${HOME}/Library/Application\ Support/Code/User/settings.json:/home/${USER}/.local/share/code-server/User/settings.json:Z \
+-e DOCKER_USER=${USER} \
+-e HOME=/home/${USER} \
 -e PASSWORD \
 -e TZ=America/Detroit \
--e PASSWORD \
+-e USER=${USER} \
 ghcr.io/charlesthomas/dev-box:${VERSION} \
-&>>  /tmp/code-server.log &
+&>> /tmp/code-server.log &
 
 echo $UUID
